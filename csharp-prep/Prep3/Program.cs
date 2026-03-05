d3646c73cc3c21a657f7953efeb27992939655cd
Braydon Tolman <bctolman2@gmail.com>
2026-03-04 18:18:36 -0700

    Prep 3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,27 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1, 101);
+        Console.WriteLine("I know a magic number. Can you guess what it is? ");
+
+        int guess = 0;
+
+        while (guess != magicNumber)
+        {
+                Console.WriteLine("What is your guess?");
+            guess = int.Parse(Console.ReadLine());
+            if ( guess == magicNumber)
+            {
+                Console.WriteLine("You guessed it!");
+            } else if (guess > magicNumber)
+            {
+                Console.WriteLine("Lower");
+            } else if (guess < magicNumber)
+            {
+                Console.WriteLine("Higher");
+            }
+        }
+        
     }
 }
