f10550e0e06b16ef2ac1999283873eb6e0dd4692
Braydon Tolman <bctolman2@gmail.com>
2026-03-04 17:05:47 -0700

    Prep 2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,27 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.WriteLine("What is your grade percentage? ");
+        String grade = Console.ReadLine();
+        int grade_percent = int.Parse(grade);
+        String letter_grade;
+        if (grade_percent >= 90)
+        {
+            letter_grade = "A";
+        } else if (grade_percent >= 80)
+        {
+            letter_grade = "B";
+        } else if (grade_percent >= 70)
+        {
+            letter_grade = "C";
+        } else if (grade_percent >= 60)
+        {
+            letter_grade = "D";
+        } else
+        {
+            letter_grade = "F";
+        }
+
+        Console.WriteLine(letter_grade);
     }
 }
