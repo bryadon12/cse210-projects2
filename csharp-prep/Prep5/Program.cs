a71a23c5697c9beb5ddbcf46378cf222df1b2653
Braydon Tolman <bctolman2@gmail.com>
2026-03-04 20:04:38 -0700

    prep 5

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -1,9 +1,51 @@
 using System;
+using System.ComponentModel.DataAnnotations;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        DisplayWelcome();
+        string name = PromptUserName();
+        int number = PromptUserNumber();
+        int year = PromtUserBirthYear();
+
+
+    }
+
+    public static void DisplayWelcome()
+    {
+        Console.WriteLine("Welcom to the program!");
+    }
+
+    public static string PromptUserName ()
+    {
+        Console.WriteLine("What is your name?");
+        string userName = Console.ReadLine();
+        return userName;
+    }
+
+    public static int PromptUserNumber()
+    {
+        Console.WriteLine("What is your facorite number? ");
+        int number = int.Parse(Console.ReadLine());
+        return number;
+    }
+
+    public static int PromtUserBirthYear()
+    {
+        Console.WriteLine("What is your birth year?");
+        return int.Parse(Console.ReadLine());
+    }
+
+    public static int SquareNumber(int number)
+    {
+        return number*number;
+    }
+
+    public static void DisplayResult(string name, int square, int year)
+    {
+        Console.WriteLine($"{name}, the square of your number is {square}.");
+        Console.WriteLine($"{name}, you will turn {2026 - year} years old this year.");
     }
 }
